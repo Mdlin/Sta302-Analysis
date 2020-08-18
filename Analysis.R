1670af34d4cba67793db06abeaaed45b71d26a9a
mifar <mifar@LAPTOP-42DMAKPP>
2020-08-19 00:02:26 +0900

    commit msg

--- Analysis.R
+++ Analysis.R
@@ -1,7 +1,23 @@
 library(MASS)
-# Fit the full model 
-full.model <- lm(Fertility ~., data = swiss)
-# Stepwise regression model
-step.model <- stepAIC(full.model, direction = "both", 
-                      trace = FALSE)
-summary(step.model)
+carprice = datasets_383055_741735_CarPrice_Assignment
+
+
+
+set.seed(100)
+Fitstart = lm(price ~ ., data = carprice)
+head(carprice)
+summary(Fitstart)
+
+step = stepAIC(Fitstart, direction = "both")
+step$anova 
+summary(step)
+
+step1=step(step, direction = "both", test = "F")
+summary(step1)
+
+step2 <- update(step1,~. - curbweight)
+summary(step2)
+
+step3 <- update(step2,~. - carwidth)
+summary(step3)
+
